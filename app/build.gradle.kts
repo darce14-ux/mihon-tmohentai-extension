bf3950ab39119a0b7336fa476e5609f93b571404
darce14-ux <darce8008@gmail.com>
2026-06-07 14:30:53 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,4 +28,8 @@ android {
 dependencies {
     implementation("org.jsoup:jsoup:1.16.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    
+    // Librerías esenciales que necesita el código de TMOHentai para no dar errores
+    implementation("io.reactivex:rxjava:1.3.8")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
