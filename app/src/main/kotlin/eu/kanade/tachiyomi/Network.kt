191bd86448ac779814e31b1a1c05e91c177ce95f
darce14-ux <darce8008@gmail.com>
2026-06-07 14:51:30 -0700

    Create Network.kt

---
+++ app/src/main/kotlin/eu/kanade/tachiyomi/Network.kt
@@ -0,0 +1,8 @@
+package eu.kanade.tachiyomi.network
+
+import okhttp3.Request
+import okhttp3.Headers
+
+fun GET(url: String, headers: Headers = Headers.Builder().build()): Request {
+    return Request.Builder().url(url).headers(headers).build()
+}
