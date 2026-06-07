3434f2e368d47e8816f5ff6fc80c57ffaa769037
darce14-ux <darce8008@gmail.com>
2026-06-07 13:42:18 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,5 +19,5 @@ android {
 dependencies {
     implementation("org.jsoup:jsoup:1.16.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    compileOnly("eu.kanade.tachiyomi:source-api:1.1")
+    compileOnly("org.tachiyomiorg:source-api:1.5")
 }
