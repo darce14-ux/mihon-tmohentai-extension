c2b9c09d9700fd10f2fae76c93673d65e0b71bce
darce14-ux <darce8008@gmail.com>
2026-06-07 14:47:53 -0700

    Update Source.kt

--- app/src/main/kotlin/eu/kanade/tachiyomi/Source.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/Source.kt
@@ -1,115 +1,30 @@
-package eu.kanade.tachiyomi
+package eu.kanade.tachiyomi.source
 
 import okhttp3.Headers
 import okhttp3.Request
 import okhttp3.Response
 
-// 1. Estructura de Redes (Network) exigida en las líneas superiores de tu código
-package eu.kanade.tachiyomi.network {
-    import okhttp3.Request
-    import okhttp3.Headers
-    fun GET(url: String, headers: Headers = Headers.Builder().build()): Request {
-        return Request.Builder().url(url).headers(headers).build()
-    }
+// --- 1. CLASES BASE DE FUENTES ---
+abstract class Source {
+    abstract val id: Long
+    abstract val name: String
+    abstract val lang: String
 }
 
-// 2. Modelos de datos del catálogo (Source Model)
-package eu.kanade.tachiyomi.source.model {
-    interface SManga {
-        var url: String
-        var title: String
-        var artist: String?
-        var author: String?
-        var description: String?
-        var genre: String?
-        var status: Int
-        var thumbnail_url: String?
-        var initialized: Boolean
-        companion object {
-            const val UNKNOWN = 0
-            const val ONGOING = 1
-            const val COMPLETED = 2
-            const val LICENSED = 3
-            fun create(): SManga = object : SManga {
-                override var url: String = ""
-                override var title: String = ""
-                override var artist: String? = null
-                override var author: String? = null
-                override var description: String? = null
-                override var genre: String? = null
-                override var status: Int = 0
-                override var thumbnail_url: String? = null
-                override var initialized: Boolean = false
-            }
-        }
-    }
+abstract class HttpSource : Source() {
+    abstract val baseUrl: String
+    open val supportsLatest: Boolean = true
+    open val headers: Headers = Headers.Builder().build()
 
-    interface SChapter {
-        var url: String
-        var name: String
-        var date_upload: Long
-        var chapter_number: Float
-        var scanlator: String?
-        companion object {
-            fun create(): SChapter = object : SChapter {
-                override var url: String = ""
-                override var name: String = ""
-                override var date_upload: Long = 0L
-                override var chapter_number: Float = -1f
-                override var scanlator: String? = null
-            }
-        }
-    }
-
-    interface Page {
-        val index: Int
-        val url: String
-        val imageUrl: String?
-        companion object {
-            fun create(index: Int, url: String, imageUrl: String?): Page = object : Page {
-                override val index: Int = index
-                override val url: String = url
-                override val imageUrl: String? = imageUrl
-            }
-        }
-    }
-
-    data class MangasPage(val mangas: List<SManga>, val hasNextPage: Boolean)
-    open class Filter<T>(val name: String, val state: T)
-    class FilterList(val list: List<Filter<*>>) : List<Filter<*>> by list
-}
-
-// 3. Clases Base de Fuentes (Source)
-package eu.kanade.tachiyomi.source {
-    import okhttp3.Headers
-    import okhttp3.Response
-    import eu.kanade.tachiyomi.source.model.MangasPage
-    import eu.kanade.tachiyomi.source.model.SManga
-    import eu.kanade.tachiyomi.source.model.SChapter
-    import eu.kanade.tachiyomi.source.model.Page
-    import eu.kanade.tachiyomi.source.model.FilterList
-
-    abstract class Source {
-        abstract val id: Long
-        abstract val name: String
-        abstract val lang: String
-    }
-
-    abstract class HttpSource : Source() {
-        abstract val baseUrl: String
-        open val supportsLatest: Boolean = true
-        open val headers: Headers = Headers.Builder().build()
-
-        abstract fun popularMangaRequest(page: Int): okhttp3.Request
-        abstract fun popularMangaParse(response: Response): MangasPage
-        abstract fun latestUpdatesRequest(page: Int): okhttp3.Request
-        abstract fun latestUpdatesParse(response: Response): MangasPage
-        abstract fun searchMangaRequest(page: Int, query: String, filters: FilterList): okhttp3.Request
-        abstract fun searchMangaParse(response: Response): MangasPage
-        abstract fun mangaDetailsParse(response: Response): SManga
-        abstract fun chapterListParse(response: Response): List<SChapter>
-        abstract fun pageListRequest(chapter: SChapter): okhttp3.Request
-        abstract fun pageListParse(response: Response): List<Page>
-        abstract fun imageUrlParse(response: Response): String
-    }
+    abstract fun popularMangaRequest(page: Int): Request
+    abstract fun popularMangaParse(response: Response): eu.kanade.tachiyomi.source.model.MangasPage
+    abstract fun latestUpdatesRequest(page: Int): Request
+    abstract fun latestUpdatesParse(response: Response): eu.kanade.tachiyomi.source.model.MangasPage
+    abstract fun searchMangaRequest(page: Int, query: String, filters: eu.kanade.tachiyomi.source.model.FilterList): Request
+    abstract fun searchMangaParse(response: Response): eu.kanade.tachiyomi.source.model.MangasPage
+    abstract fun mangaDetailsParse(response: Response): eu.kanade.tachiyomi.source.model.SManga
+    abstract fun chapterListParse(response: Response): List<eu.kanade.tachiyomi.source.model.SChapter>
+    abstract fun pageListRequest(chapter: eu.kanade.tachiyomi.source.model.SChapter): Request
+    abstract fun pageListParse(response: Response): List<eu.kanade.tachiyomi.source.model.Page>
+    abstract fun imageUrlParse(response: Response): String
 }
