6e7393ddcfb2567eef41699406de1140f9394da2
darce14-ux <darce8008@gmail.com>
2026-06-07 15:31:18 -0700

    Update Network.kt

--- app/src/main/kotlin/eu/kanade/tachiyomi/Network.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/Network.kt
@@ -1,8 +1,9 @@
-package eu.kanade.tachiyomi
+package eu.kanade.tachiyomi.network
 
 import okhttp3.Request
 import okhttp3.Headers
 
+// Genera una petición HTTP estándar tipo GET usando la librería OkHttp3
 fun GET(url: String, headers: Headers = Headers.Builder().build()): Request {
     return Request.Builder().url(url).headers(headers).build()
 }
