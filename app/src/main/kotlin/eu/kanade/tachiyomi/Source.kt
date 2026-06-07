118f9432cc823db901da84dafffbc9462af7ab7e
darce14-ux <darce8008@gmail.com>
2026-06-07 14:56:29 -0700

    Update Source.kt

--- app/src/main/kotlin/eu/kanade/tachiyomi/Source.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/Source.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.source
+package eu.kanade.tachiyomi
 
 import okhttp3.Headers
 import okhttp3.Request
@@ -16,14 +16,14 @@ abstract class HttpSource : Source() {
     open val headers: Headers = Headers.Builder().build()
 
     abstract fun popularMangaRequest(page: Int): Request
-    abstract fun popularMangaParse(response: Response): eu.kanade.tachiyomi.source.model.MangasPage
+    abstract fun popularMangaParse(response: Response): MangasPage
     abstract fun latestUpdatesRequest(page: Int): Request
-    abstract fun latestUpdatesParse(response: Response): eu.kanade.tachiyomi.source.model.MangasPage
-    abstract fun searchMangaRequest(page: Int, query: String, filters: eu.kanade.tachiyomi.source.model.FilterList): Request
-    abstract fun searchMangaParse(response: Response): eu.kanade.tachiyomi.source.model.MangasPage
-    abstract fun mangaDetailsParse(response: Response): eu.kanade.tachiyomi.source.model.SManga
-    abstract fun chapterListParse(response: Response): List<eu.kanade.tachiyomi.source.model.SChapter>
-    abstract fun pageListRequest(chapter: eu.kanade.tachiyomi.source.model.SChapter): Request
-    abstract fun pageListParse(response: Response): List<eu.kanade.tachiyomi.source.model.Page>
+    abstract fun latestUpdatesParse(response: Response): MangasPage
+    abstract fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request
+    abstract fun searchMangaParse(response: Response): MangasPage
+    abstract fun mangaDetailsParse(response: Response): SManga
+    abstract fun chapterListParse(response: Response): List<SChapter>
+    abstract fun pageListRequest(chapter: SChapter): Request
+    abstract fun pageListParse(response: Response): List<Page>
     abstract fun imageUrlParse(response: Response): String
 }
